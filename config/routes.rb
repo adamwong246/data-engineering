3113c117ed02a73f1510c832f5af919278490282
Adam Wong <adamwong246@gmail.com>
2014-04-06 14:21:43 -0700

    added root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 AwongTestLivingSocial::Application.routes.draw do
   resources :merchants
-
-
   resources :items
-
-
   resources :purchasers
-
-
   resources :purchases
 
-
+  root :to => "purchases#new"
 end
